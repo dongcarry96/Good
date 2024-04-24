b06e531cc9fcd6f9e3ddd2ba1bded356c9ef4c76
dong <fhakwpak6@gmail.com>
2024-04-24 19:46:09 +0900

    Feat: Item 서비스 추가

---
+++ src/main/java/com/dong/good/Item/service/ItemService.java
@@ -0,0 +1,29 @@
+package com.dong.good.Item.service;
+
+import com.dong.good.Item.domain.Item;
+import com.dong.good.Item.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
